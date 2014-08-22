65dfcec3886dc5dabf0c84d711d4c4f66403a752
Valnar Dewan <mail@story75.com>
2014-08-22 18:26:58 +0200

    [TASK] CLear Bonefish

--- src/Autoloader/bootstrap.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-require 'Autoloader.php';
-
-$autoloader = new Bonefish\Autoloader\Autoloader();
-$autoloader->register();
-
-$autoloader->addNamespace('Bonefish','src');
-$autoloader->addNamespace('Bonefish\Tests','tests');
--- src/bootstrap.php
+++
@@ -1,3 +0,0 @@
-<?php
-
-require 'Autoloader/bootstrap.php';
