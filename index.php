e3a9be521d7c806c147d9d7d47be4dcfb2c136e0
Valnar Dewan <mail@story75.com>
2014-08-27 23:03:52 +0200

    [TASK] Add require for vendor autoload

--- index.php
+++ index.php
@@ -1,7 +1,14 @@
 <?php
+
 /**
- * Created by PhpStorm.
- * User: Alexander
- * Date: 27.08.14
- * Time: 20:21
- */ 
+ * This software is provided 'as-is', without any express or implied warranty.
+ * In no event will the authors be held liable for any damages arising from the use of this software.
+ * @author     Alexander Schmidt <mail@story75.com>
+ * @copyright  Copyright (c) 2014, Alexander Schmidt
+ * @version    1.0
+ * @date       2014-08-27
+ * @package    Bonefish
+ */
+
+require 'vendor/autoload.php';
+
